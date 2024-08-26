68b07af11ac358713a2b02838509d01223d74b17
Zasetsu <g140302044@sakarya.edu.tr>
2024-08-26 10:12:52 +0300

    Update closure.blade.php

--- resources/views/dashboard/pages/closure.blade.php
+++ resources/views/dashboard/pages/closure.blade.php
@@ -96,12 +96,18 @@ use Carbon\Carbon;
                                         @endif
                                         >
                                             <option selected disabled> Ay Seçin</option>
-                                            @for ($i = 1; $i <= 12; $i++)
-                                                @if ($i <= date('m'))
-                                                    <option value="{{ $i }}">
-                                                        {{ strftime('%B', mktime(0, 0, 0, $i, 1)) }}</option>
-                                                @endif
-                                            @endfor
+                                            <option value="1">Ocak</option>
+                                            <option value="2">Şubat</option>
+                                            <option value="3">Mart</option>
+                                            <option value="4">Nisan</option>
+                                            <option value="5">Mayıs</option>
+                                            <option value="6">Haziran</option>
+                                            <option value="7">Temmuz</option>
+                                            <option value="8">Ağustos</option>
+                                            <option value="9">Eylül</option>
+                                            <option value="10">Ekim</option>
+                                            <option value="11">Kasım</option>
+                                            <option value="12">Aralık</option>
 
                                         </select>
                                     </div>
